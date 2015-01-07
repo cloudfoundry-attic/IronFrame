91d0c753e9b67ef44ae1a01dc803db1d15c4787d
mosoto <marcel.ortiz@attackpattern.com>
2015-01-06 16:30:41 -0800

    1. Update xUnit Version 2. Add an Environment dictionary to the TaskCommandDTO message.

---
+++ IronFoundry.Warden.Test/Tasks/TaskCommandDTODeserializaitonTests.cs
@@ -0,0 +1,51 @@
+﻿
+namespace IronFoundry.Warden.Test.Tasks
+{
+    using System.IO;
+    using FluentAssertions;
+    using IronFoundry.Warden.Tasks;
+    using Xunit;
+    using Newtonsoft.Json;
+
+    public class TaskCommandDTODeserializationTests
+    {
+        /// <summary>
+        /// Verify that the old message containing only cmd and args continues to be deserialized correctly.
+        /// </summary>
+        [Fact]
+        public void WhenOldMessageDeserialized()
+        {
+            string json = @"{ cmd: ""foo"", args: [ ""arg1"", ""arg2""] }";
+            var task = DeserializeJson(json);
+
+            task.Command.Should().Be("foo");
+            task.Args.Should().BeEquivalentTo(new[] {"arg1", "arg2"});
+            task.Environment.Should().BeNull();
+        }
+
+        /// <summary>
+        /// Verify that the new messages containing the environment that should be used with the invoke
+        /// can be deserialized correctly.
+        /// </summary>
+        [Fact]
+        public void WhenMessageWithEnvironmentDeserialized()
+        {
+            string json = @"{ cmd: ""foo"", args: [ ""arg1"", ""arg2""], env: { env1: ""val1"", env2: ""val2"" } }";
+            var task = DeserializeJson(json);
+
+            task.Command.Should().Be("foo");
+            task.Args.Should().BeEquivalentTo(new[] { "arg1", "arg2" });
+            task.Environment.Should().HaveCount(2);
+            task.Environment.Should().HaveCount(2).And.Contain("env1", "val1").And.Contain("env2", "val2");
+        }
+
+        TaskCommandDTO DeserializeJson(string json)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            var textReader = new JsonTextReader(new StringReader(json));
+            var task = serializer.Deserialize<TaskCommandDTO>(textReader);
+            return task;
+        }
+
+    }
+}
