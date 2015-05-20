16d3cae82236048b7efbf980078f2c0304be3ad8
Ben Moss & David Morhovich <pair+dmorhovich+bmoss@pivotal.io>
2015-05-20 10:51:03 -0400

    Introduce DiskQuotaManager to be able to stub in the tests
    The introduction of disk quotas caused some tests to fail as
    container.Destroy() was trying to remove the quota of a non existing
    user.
    
    [Fix #95051518]

---
+++ IronFrame/Utilities/DiskQuotaManager.cs
@@ -0,0 +1,19 @@
+﻿using DiskQuotaTypeLibrary;
+
+namespace IronFrame.Utilities
+{
+    internal interface IDiskQuotaManager
+    {
+        DiskQuotaControl CreateDiskQuotaControl(IContainerDirectory dir);
+    }
+
+    internal class DiskQuotaManager : IDiskQuotaManager
+    {
+        public DiskQuotaControl CreateDiskQuotaControl(IContainerDirectory dir)
+        {
+            var diskQuotaControl = new DiskQuotaControl();
+            diskQuotaControl.Initialize(dir.Volume, true);
+            return diskQuotaControl;
+        }
+    }
+}
